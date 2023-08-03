18f9aa113aa1c8b1eb797751dd9d806faf6aaa7e
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-08-03 11:00:10 -0300

    .

---
+++ dowhile.js
@@ -0,0 +1,8 @@
+// Usando DO... WHILE
+ 
+i = 0               // declaração do valor de início
+ 
+do{        
+    console.log(i);  // imprime os dados na tela
+    i++;             // incrementação: +1
+}while(i < -1);       // condição de parada
