4433e34199bf0a6effa3f23588d90825ed3098b9
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-08-03 10:17:40 -0300

    .

---
+++ parada.js
@@ -0,0 +1,15 @@
+const cont = 10;
+
+for(i = 0; i < cont; i++){
+    if(i == 7){
+        break;
+    }
+    console.log(i);
+} 
+
+/*for(i = 0; i < 5; i++){
+    if(i == 2){         // se i é igual a 2
+      break;            // pare imediatamente
+    }
+    console.log(i);
+  }*/
