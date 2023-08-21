770fcc3860eb60e14de8f759258ac302926ba22c
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2023-08-21 10:39:22 -0300

    .

---
+++ provas.js
@@ -0,0 +1,8 @@
+for(i = 0; i < 5; i++){
+
+    if(i % 2 == 0){
+        break;
+    }
+    console.log(i);
+
+}
---
+++ teste.js
@@ -0,0 +1,8 @@
+const n = 5;
+
+for(i = 0; i < n; i++){
+    if(i % 3 == 0) {
+        break;
+    }
+    console.log(i);
+}
